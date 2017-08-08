85b37c66d07c6123c05702c17fb15a645ba5ea23
platinumwb <ojramos@platinumwb.com>
2017-08-08 17:02:33 -0400

    route change

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,21 @@
 
 //Route::get('ezlogin', ['as' => 'ezlogin', 'uses' => '\App\Http\Controllers\EZLoginController@index']);
 
+Auth::routes();
+
 Route::get('/', ['as' => 'root', 'uses' => '\App\Http\Controllers\Auth\LoginController@login']);
+//Route::get('login', ['as' => 'root', 'uses' => '\App\Http\Controllers\Auth\LoginController@login']);
 Route::get('dashboard', ['as' => 'samples', 'uses' => '\App\Http\Controllers\DashboardController@index']);
 Route::get('forms', ['as' => 'forms', 'uses' => '\App\Http\Controllers\FormsController@index']);
 Route::get('tables', ['as' => 'tables', 'uses' => '\App\Http\Controllers\TablesController@index']);
 
 
 Route::group(['prefix' => 'users'], function () {
+
     Route::get('/', ['as' => 'users', 'uses' => '\App\Http\Controllers\UsersController@index']);
     Route::get('create', ['as' => 'create.user', 'uses' => '\App\Http\Controllers\UsersController@createUser']);
     Route::get('logout', ['as' => 'logout.user', 'uses' => '\App\Http\Controllers\UsersController@logoutUser']);
+
 });
 
 Route::group(['prefix' => 'vendors'], function () {
@@ -39,6 +44,6 @@ Route::group(['prefix' => 'vendors'], function () {
 });
 
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+//Route::get('/home', 'HomeController@index')->name('home');
